011f693076aa7ad1465d355eae2c78663fca5e71
GabbereX <[gabberex@mail.ru]>
2022-07-03 16:29:38 +0300

    Updates

---
+++ scripts_8be7d2c09e9e4b4465b4.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.getElementById("burgerMenuButton"),t=document.getElementById("responsiveMenu"),n=document.getElementById("closeMenuButton");e.addEventListener("click",(()=>{t.style.transform="translateY(0)",document.body.style.overflowY="hidden"})),n.addEventListener("click",(()=>{t.style.transform="translateY(-100%)",document.body.style.overflowY=""}))})();
